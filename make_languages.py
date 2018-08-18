a0b54b104a8f179de8e925d30b1ced4a2d045683
Daniel CAUNE <daniel.caune@gmail.com>
2018-08-18 21:13:14 +0700

    Use the stdin to read the content of the CSV file "Language ISO Code".

--- make_languages.py
+++ make_languages.py
@@ -22,26 +22,21 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import argparse
 import collections
 import csv
 import json
 import os
+import sys
 
 
 # Name of the column identifying the ISO 639-3 code of each language.
 CSV_FIELD_ISO_639_3 = 'ISO 639-3:2007'
 
 
-def build_languages_localized_names(language_csv_file_path_name):
+def build_languages_localized_names():
     """
     Build the localized names of the languages defined in the given CSV
-    file.
-
-
-    @param language_csv_file_path_name: absolute path and name of the
-        Comma-Separated Values (CSV) file that contains the localized
-        names of languages identified with ISO 639-3 codes.
+    content read from the standard input.
 
 
     @return: a dictionary for each language identified with a ISO 639-3
@@ -59,41 +54,21 @@ def build_languages_localized_names(language_csv_file_path_name):
     """
     languages_localized_names = collections.defaultdict(dict)
 
-    with open(language_csv_file_path_name, 'rb') as file:
-        for row in csv.DictReader(file):
-            languages_localized_names[row[CSV_FIELD_ISO_639_3]] = dict([(label_iso_code, label)
-                    for (label_iso_code, label) in row.iteritems()
-                        if label_iso_code != CSV_FIELD_ISO_639_3])
+    for row in csv.DictReader(sys.stdin):
+        languages_localized_names[row[CSV_FIELD_ISO_639_3]] = dict([(label_iso_code, label)
+                for (label_iso_code, label) in row.iteritems()
+                    if label_iso_code != CSV_FIELD_ISO_639_3])
 
     return languages_localized_names
 
 
 def main():
     """
-    Read the arguments passed within the command line and convert the
-    specified CSV file containing a list of languages and their localized
-    names, into a JSON expression printed to the standard output.
+    Convert the content CSV file--read from the standard input--containing
+    a list of languages and their localized names, into a JSON expression
+    printed to the standard output.
     """
-    arguments = parse_arguments()
-
-    print json.dumps(build_languages_localized_names(os.path.expanduser(arguments.language_csv_file_path_name)))
-
-
-def parse_arguments():
-    """
-    Convert argument strings to objects and assign them as attributes of
-    the namespace.
-
-
-    @return: an instance ``argparse.Namespace`` corresponding to the
-        populated namespace.
-    """
-    parser = argparse.ArgumentParser(description='Build the localized names of languages')
-
-    parser.add_argument('-i', '--language-csv-file', dest='language_csv_file_path_name', metavar='FILE', required=True,
-            help='absolute path of the CSV file containing the localized names of languages')
-
-    return parser.parse_args()
+    print json.dumps(build_languages_localized_names())
 
 
 if __name__ == '__main__':
